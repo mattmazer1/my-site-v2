8fd7c1e81d54bf351971699c4f9a0c4a02e4588a
Matt <84170718+mattmazer1@users.noreply.github.com>
2024-08-20 18:32:46 +1000

    Format and update data transfer vals (#1)

--- src/components/iconLink.tsx
+++ src/components/iconLink.tsx
@@ -4,18 +4,18 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 interface iconProps {
-	link: string;
-	icon: typeof faEnvelope | typeof faGithub | typeof faLinkedin;
+  link: string;
+  icon: typeof faEnvelope | typeof faGithub | typeof faLinkedin;
 }
 
 const IconLink: React.FC<iconProps> = ({ link, icon }) => {
-	return (
-		<a href={link} target="_blank" rel="noopener noreferrer">
-			<FontAwesomeIcon
-				icon={icon}
-				className="hover:text-gray-400 hover:scale-125 ml-2 mr-2"
-			></FontAwesomeIcon>
-		</a>
-	);
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon
+        icon={icon}
+        className="hover:text-gray-400 hover:scale-125 ml-2 mr-2"
+      ></FontAwesomeIcon>
+    </a>
+  );
 };
 export default IconLink;
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,17 +2,17 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 const Custom404: React.FC = () => {
-	const router = useRouter();
+  const router = useRouter();
 
-	useEffect(() => {
-		router.push("/");
-	}, []);
+  useEffect(() => {
+    router.push("/");
+  }, []);
 
-	return (
-		<div className="flex items-center justify-center h-screen">
-			<div className="text-2xl">404 not found</div>
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-2xl">404 not found</div>
+    </div>
+  );
 };
 
 export default Custom404;

84a7e014e9b132d35829c6e5b8dc33122649804e
Matt Mazer <mazermatt3@gmail.com>
2024-08-20 18:26:50 +1000

    Format and update data transfer vals

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,91 +1,72 @@
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import IconLink from "@/components/iconLink";
-import moment from "moment";
-import "moment-timezone";
 import { useEffect } from "react";
 
-export async function getStaticProps() {
-	return {
-		props: {},
-	};
-}
-
 const getIp = async (): Promise<string | null> => {
-	try {
-		const response = await fetch("https://api.ipify.org/?format=json");
-		if (response.ok) {
-			const data = await response.json();
-			return data.ip;
-		} else {
-			console.error("could not fetch ip", response.status);
-			return null;
-		}
-	} catch (error) {
-		console.error("could not fetch ip:", error);
-		return null;
-	}
-};
-
-const getDateTime = (): { date: string; time: string } => {
-	const now = moment().tz("Pacific/Auckland");
-	const localDate = now.format("DD/MM/YY");
-	const localTime = now.format("HH:mm");
-
-	return { date: localDate, time: localTime };
+  try {
+    const response = await fetch("https://api.ipify.org/?format=json");
+    if (response.ok) {
+      const data = await response.json();
+      return data.ip;
+    } else {
+      console.error("could not fetch ip", response.status);
+      return null;
+    }
+  } catch (error) {
+    console.error("could not fetch ip:", error);
+    return null;
+  }
 };
 
 const postVisit = async (): Promise<void> => {
-	let ip = await getIp();
+  let ip = await getIp();
 
-	const postData = {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({
-			data: {
-				ip: ip,
-				date: getDateTime().date,
-				time: getDateTime().time,
-			},
-		}),
-	};
-	try {
-		const response = await fetch(
-			process.env.NEXT_PUBLIC_POST_REQUEST as string,
-			postData
-		);
-		await response.json();
+  const postData = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      ip: ip,
+    }),
+  };
 
-		if (!response.ok) {
-			throw new Error("could not post visit data");
-		}
-	} catch (error: any) {
-		console.log(error);
-	}
+  try {
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_POST_REQUEST as string,
+      postData
+    );
+    await response.json();
+
+    if (!response.ok) {
+      throw new Error("could not post visit data");
+    }
+  } catch (error: any) {
+    console.log(error);
+  }
 };
 
 export default function Home() {
-	useEffect(() => {
-		postVisit();
-	}, []);
+  useEffect(() => {
+    postVisit();
+  }, []);
 
-	return (
-		<>
-			<div className=" flex flex-col flex-wrap justify-center items-center sm:mt-toph mt-tops">
-				<h1 className="sm:text-4xl text-3xl">Matt Mazer</h1>
+  return (
+    <>
+      <div className=" flex flex-col flex-wrap justify-center items-center sm:mt-toph mt-tops">
+        <h1 className="sm:text-4xl text-3xl">Matt Mazer</h1>
 
-				<h2 className="mt-4 sm:text-lg text-sm"> Software developer</h2>
-				<div className="mt-4 sm:text-6xl text-4xl">
-					<IconLink link={"https://github.com/mattmazer1"} icon={faGithub} />
+        <h2 className="mt-4 sm:text-lg text-sm">Platform engineer</h2>
+        <div className="mt-4 sm:text-6xl text-4xl">
+          <IconLink link={"https://github.com/mattmazer1"} icon={faGithub} />
 
-					<IconLink
-						link={"https://www.linkedin.com/in/matt-mazer/"}
-						icon={faLinkedin}
-					/>
+          <IconLink
+            link={"https://www.linkedin.com/in/matt-mazer/"}
+            icon={faLinkedin}
+          />
 
-					<IconLink link={"mailto:mattmazerdev@gmail.com"} icon={faEnvelope} />
-				</div>
-			</div>
-		</>
-	);
+          <IconLink link={"mailto:mattmazerdev@gmail.com"} icon={faEnvelope} />
+        </div>
+      </div>
+    </>
+  );
 }
